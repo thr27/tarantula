836d3ee803dfc764f2646e5dd4d4d80bdfd0db84
Whapow <joshuavancamp@gmail.com>
2017-06-12 15:11:32 -0400

    Tweak to run locally

--- db/migrate/005_convert_to_unicode.rb
+++ db/migrate/005_convert_to_unicode.rb
@@ -10,18 +10,18 @@ class ConvertToUnicode < ActiveRecord::Migration
 
   private
   def self.alter_database_and_tables_charsets charset = default_charset, collation = default_collation
-    case connection.adapter_name
-    when 'MySQL'
-      execute "ALTER DATABASE #{connection.current_database} CHARACTER SET #{charset} COLLATE #{collation}"
+    # case connection.adapter_name
+    # when 'MySQL'
+    #   execute "ALTER DATABASE #{connection.current_database} CHARACTER SET #{charset} COLLATE #{collation}"
 
-      connection.tables.each do |table|
-        execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET #{charset} COLLATE #{collation}"
-      end
-    else
-      # OK, not quite irreversible but can't be done if there's not
-      # the code here to support it...
-      raise ActiveRecord::IrreversibleMigration.new("Migration error: Unsupported database for migration to UTF-8 support")
-    end
+    #   connection.tables.each do |table|
+    #     execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET #{charset} COLLATE #{collation}"
+    #   end
+    # else
+    #   # OK, not quite irreversible but can't be done if there's not
+    #   # the code here to support it...
+    #   raise ActiveRecord::IrreversibleMigration.new("Migration error: Unsupported database for migration to UTF-8 support")
+    # end
   end
 
   def self.default_charset
