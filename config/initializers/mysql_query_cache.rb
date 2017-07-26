4478670289f45a5ea8bfd7c72d216ff482457b45
thr27 <robert.thurnreiter@fti.de>
2017-07-26 15:12:55 +0200

    remove query_cache_type change
    dont change mysql server settings from app, query cache is outdated in times of innodb

--- config/initializers/mysql_query_cache.rb
+++ config/initializers/mysql_query_cache.rb
@@ -1,2 +1,3 @@
 
-ActiveRecord::Base.connection.execute("SET GLOBAL query_cache_type = 1;")
+# dont change mysql server settings from app, query cache is outdated in times of innodb
+# ActiveRecord::Base.connection.execute("SET GLOBAL query_cache_type = 1;")
